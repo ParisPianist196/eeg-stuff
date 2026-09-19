048adcb7fdf3d98f1a60e21d59dfa72180565659
Abi <abigail@abigailhendrick.com>
2026-09-19 12:23:27 -0400

    Added firmware

--- firmware/firmware/firmware.ino
+++ firmware/firmware/firmware.ino
@@ -5,13 +5,7 @@ void setup() {
 }
 
 void loop() {
-  int raw = analogRead(ADC_PIN);
-
-  float voltage = raw * (5.0 / 1023.0);
-
-  Serial.print(raw);
-  Serial.print(",");
-  Serial.println(voltage, 4);
+  Serial.println(analogRead(A0));
 
   delay(10);  // ~100 samples/sec
 }
--- firmware/test_signal/test_signal.ino
+++ firmware/test_signal/test_signal.ino
@@ -1,13 +1,17 @@
-const int signalPin = 8;
+const int signalPin1 = 8;
+const int signalPin2 = 6;
 
 void setup() {
-  pinMode(signalPin, OUTPUT);
+  pinMode(signalPin1, OUTPUT);
+  pinMode(signalPin2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(signalPin, HIGH);
+  digitalWrite(signalPin1, HIGH);
+  digitalWrite(signalPin2, HIGH);
   delay(250);
 
-  digitalWrite(signalPin, LOW);
+  digitalWrite(signalPin1, LOW);
+  digitalWrite(signalPin2, LOW);
   delay(250);
 }
